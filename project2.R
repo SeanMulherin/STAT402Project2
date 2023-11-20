1d2672a590be3ad476b35856785fd693f2a6edc7
Sean  Mulherin <145077678+SeanMulherin@users.noreply.github.com>
2023-11-20 11:15:17 -0800

    Add files via upload

--- project2.R
+++ project2.R
@@ -1,16 +1,39 @@
-##### Analyzing The Influence of Biological Factors vs. Lifestyle Habits on Stroke Victims #####
+##### What are the most influential predictors for Stroke #####
 # Biological Factors: gender, age, hypertension, heart_disease, avg_glucose_level, bmi
 # Lifestyle Habits: ever_married, work_type, Residence_type, smoking_status 
 
 library(ggplot2)
 library(dplyr)
+library(tidyverse)
+install.packages("GGally"); library(GGally) #scatterplot matrix
+install.packages("corrplot"); library(corrplot) #correlation heat map
 
-stroke <- read.csv("healthcare-dataset-stroke-data.csv", header = T, na.strings = "N/A") |> na.omit()
+stroke_df <- read.csv("healthcare-dataset-stroke-data.csv", header = T, na.strings = "N/A") |> na.omit()
+stroke_df_numeric <- stroke_df |> select(c("age", "hypertension", "heart_disease", "avg_glucose_level", "bmi", "stroke"))
 
-####### Biological Variable Selection ####### 
-b1 <- lm(stroke ~ gender + age + hypertension + heart_disease + avg_glucose_level + bmi, data = stroke)
-summary(m1)
+####### EDA ####### 
+ggplot(stroke_df) +
+  geom_bar(mapping = aes(x = age), width = 2) + theme_classic() + ggtitle("Age")
+ggplot(stroke_df) +
+  geom_bar(mapping = aes(x = avg_glucose_level), width=15) + theme_classic() + ggtitle("Glucose Level")
+ggplot(stroke_df) +
+  geom_bar(mapping = aes(x = bmi), width = 10) + theme_classic() + ggtitle("BMI")
 
-####### Lifestyle Variable Selection ####### 
-l1 <- lm(stroke ~ ever_married + work_type + Residence_type + smoking_status, data = stroke)
-summary(l1)
+# correlation heat map
+corrplot.mixed(cor(stroke_df_numeric),
+               lower = "number", upper = "circle", tl.pos = "l", diag = "l",
+               main = "Correlation Matrix")
+
+
+####### Variable Selection ####### 
+m1 <- glm(stroke ~ ., data = stroke, family = binomial) # logistic regression
+summary(m1) # significant vars include: age, hypertension, avg_glucose_level
+plot(m1)
+
+ggpairs(stroke, columns = c(12, 2, 3, 8)) # scatterplot matrix for sig vars
+
+####### Check for colinearity with VIF
+
+
+
+
