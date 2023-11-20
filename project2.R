f05c5ea9b6ed96656c9dbd0f184afbd3220bba5e
Sean  Mulherin <145077678+SeanMulherin@users.noreply.github.com>
2023-11-20 15:59:51 -0800

    Add files via upload

--- project2.R
+++ project2.R
@@ -5,19 +5,25 @@
 library(ggplot2)
 library(dplyr)
 library(tidyverse)
-install.packages("GGally"); library(GGally) #scatterplot matrix
-install.packages("corrplot"); library(corrplot) #correlation heat map
+library(car)
+library(GGally) #scatterplot matrix
+library(corrplot) #correlation heat map
 
 stroke_df <- read.csv("healthcare-dataset-stroke-data.csv", header = T, na.strings = "N/A") |> na.omit()
 stroke_df_numeric <- stroke_df |> select(c("age", "hypertension", "heart_disease", "avg_glucose_level", "bmi", "stroke"))
 
-####### EDA ####### 
+############################ EDA ############################ 
+# histograms for numeric vars
 ggplot(stroke_df) +
   geom_bar(mapping = aes(x = age), width = 2) + theme_classic() + ggtitle("Age")
 ggplot(stroke_df) +
-  geom_bar(mapping = aes(x = avg_glucose_level), width=15) + theme_classic() + ggtitle("Glucose Level")
+  geom_bar(mapping = aes(x = avg_glucose_level), width = .001) + theme_classic() + ggtitle("Glucose Level")
 ggplot(stroke_df) +
   geom_bar(mapping = aes(x = bmi), width = 10) + theme_classic() + ggtitle("BMI")
+
+# transform avg_glucose_level because it's right skewed
+symbox(~avg_glucose_level, data = stroke_df)
+stroke_df$avg_glucose_level <- avg_glucose_level^(-1)
 
 # correlation heat map
 corrplot.mixed(cor(stroke_df_numeric),
