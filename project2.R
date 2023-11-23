51676fa14b119f074931765c6bb399bd5ce8f546
Dan Knight <danknight@mednet.ucla.edu>
2023-11-22 18:29:16 -0800

    Split train/test data

--- project2.R
+++ project2.R
@@ -2,10 +2,34 @@
 # Biological Factors: gender, age, hypertension, heart_disease, avg_glucose_level, bmi
 # Lifestyle Habits: ever_married, work_type, Residence_type, smoking_status 
 
-library(ggplot2)
-library(dplyr)
+set.seed(10);
+library(ggplot2);
+library(dplyr);
 
-stroke <- read.csv("healthcare-dataset-stroke-data.csv", header = T, na.strings = "N/A") |> na.omit()
+
+load_stroke_data <- function(path) {
+    read.csv(path, header = T, na.strings = "N/A");
+}
+
+prep_stroke_data <- function(stroke_data, training_proportion) {
+    stroke_data <- na.omit(stroke_data);
+    
+    n <- nrow(stroke_data);
+    num_training <- n * training_proportion;
+    training_i <- sample.int(n, num_training);
+    
+    list(
+        training = stroke_data[training_i, ],
+        test = stroke_data[-training_i, ]
+    );
+}
+
+stroke <- prep_stroke_data(
+    load_stroke_data("healthcare-dataset-stroke-data.csv"),
+    training_proportion = 0.8
+);
+
+stroke
 
 ####### Biological Variable Selection ####### 
 b1 <- lm(stroke ~ gender + age + hypertension + heart_disease + avg_glucose_level + bmi, data = stroke)
